a6ca405bd6e1c651ada8717fea2f9d550db30d48
Andres Felipe Camargo Lastra <ingcamargolastra@gmail.com>
2019-09-20 19:54:14 -0500

    Factorial de un número

--- divisas.py
+++ divisas.py
@@ -10,7 +10,7 @@ def run():
 
 def menu():
     opcion = 1
-    while opcion ==1:
+    while opcion == 1:
         print('1. Divisas')
         print('2. Factorial')
         print('3. Primo')
@@ -19,6 +19,17 @@ def menu():
         opcion = float(input('Digite la opcion: '))
         if(opcion == 1):
             run()
+        elif opcion == 2:
+            n = float(input('Ingrese el número: '))
+            total = factorial(n)
+            print('El factorial de {} es {} '.format(n,total))
+            
+def factorial(numero):
+    if (numero == 0):
+        return 1
+    return numero * factorial(numero - 1)
+
+
 
 if __name__ == '__main__':
     menu()
