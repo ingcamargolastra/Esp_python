dec39a8ac77fc5624da5bbeceaceeca904441a5d
Andres Felipe Camargo Lastra <ingcamargolastra@gmail.com>
2019-09-20 19:47:03 -0500

    Agregando el menu al projecto

--- divisas.py
+++ divisas.py
@@ -8,9 +8,20 @@ def run():
     resultado = calcular_divisas(cantidad)
     print('{} dolares son ${} pesos colombianos'.format(cantidad, resultado))
 
+def menu():
+    opcion = 1
+    while opcion ==1:
+        print('1. Divisas')
+        print('2. Factorial')
+        print('3. Primo')
+        print('4. Busqueda Binaria')
+        print('5. Final')
+        opcion = float(input('Digite la opcion: '))
+        if(opcion == 1):
+            run()
 
 if __name__ == '__main__':
-    run()
+    menu()
     print('Final')
 
     
